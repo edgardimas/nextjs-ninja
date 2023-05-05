dc6706178e5b41312fdaee36e2c6991c16d60e57
Edgar Dimas <edgar.dimas.ir@gmail.com>
2023-05-05 15:52:28 +0700

    first commit

---
+++ comps/Navbar.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <Image src="/logo.png" alt="logo" width={128} height={77} />
+      </div>
+      <Link href="/">
+        <p>Home</p>
+      </Link>
+      <Link href="/about">
+        <p>About</p>
+      </Link>
+      <Link href="/ninjas">
+        <p>Ninja Listing</p>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ pages/about.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title> Ninja List | About </title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi
+          non officiis, aliquid enim fuga ab! Aliquid perferendis, temporibus
+          provident maxime quis in asperiores error ullam, voluptates iure
+          ratione aliquam autem.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi fugit
+          dicta amet corporis libero numquam deleniti facere excepturi,
+          assumenda culpa accusantium vero eum necessitatibus quam accusamus est
+          ut blanditiis consectetur!
+        </p>
+      </div>
+    </>
+  );
+}
